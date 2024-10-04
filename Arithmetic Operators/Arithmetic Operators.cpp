7b713159c05fc18e9a29d5710c55525958091e95
Sam Hart <81168131+SamH5858@users.noreply.github.com>
2024-10-04 12:07:04 +0100

    Lecture 2 update
    Main tutorial 2 content completed

---
+++ Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,21 @@
+// Arithmetic Operators.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << 5+5 << "\n";
+    std::cout << 355e0f / 113e0f << "\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
