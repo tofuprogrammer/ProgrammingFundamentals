f2095a8131e403276c8bc43b1d068ab4952b3e5f
h011771o <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-01 11:44:22 +0000

    Finished Exercise 2 code

--- Exercise 2/Exercise 2.cpp
+++ Exercise 2/Exercise 2.cpp
@@ -10,4 +10,20 @@ int main()
 {
     int numbers[] = {3, 4, 5};
     printArray(numbers, sizeof(numbers));
-} */
+}
+iostream is not included
+sizeof gives the size in memory of the item being referred to so the function goes out of bounds */
+
+#include <iostream>
+
+void printArray(int* arr, int size)
+{
+    for (int i = 0; i < size; i++)
+        std::cout << arr[i] << '\n';
+}
+
+int main()
+{
+    int numbers[] = {3, 4, 5};
+    printArray(numbers, std::size(numbers));
+}
