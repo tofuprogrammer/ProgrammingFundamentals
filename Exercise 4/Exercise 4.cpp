398dbd532ea2e557162fe3de2948f82a6df71fa5
h011771o <81168131+tofuprogrammer@users.noreply.github.com>
2024-11-01 12:47:46 +0000

    Completed Exercise 4

--- Exercise 4/Exercise 4.cpp
+++ Exercise 4/Exercise 4.cpp
@@ -24,6 +24,7 @@ iostream not included
 no int main()
 => is not a valid expression, >= should be used instead
 ; should not be at the end of the for loop
+No '\n' at the end of the output string
 No ; at the end of the return statement */
 
 #include <iostream>
@@ -44,7 +45,13 @@ bool isLevelOver(int playerScore, Stage* stages, int stagesSize)
     {
         if (stages[i].completed)
             count += 1;
-        std::cout << "stage " << i << " completed!";
+        std::cout << "stage " << i << " completed!\n";
     }
-    return (hasEnoughScore & count >= 10);
+    return (hasEnoughScore & (count >= 10));
+}
+
+int main()
+{
+    Stage stages[] = {{0, true}, {1, true}, {2, false}};
+    isLevelOver(430, stages, 3);
 }
